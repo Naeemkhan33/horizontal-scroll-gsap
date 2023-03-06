c33028b2b2beb7cf55f045472dd66e07f3b17337
unknown <ranker333@gmail.com>
2023-03-06 16:03:51 +0500

    dasd

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withTM = require("next-transpile-modules")(["three"]);
 
 module.exports = {
   reactStrictMode: true,
+  transpilePackages: ["example-package"], // add your transpiled packages here
   images: {
     domains: ["tailwindui.com", "images.unsplash.com"],
   },
